81c8ffdab3449688b6c3df81401db4d8d64b10a4
Manu Hdez <manu.hdez.dev@gmail.com>
2019-11-25 23:23:53 +0000

    using type definition files

--- src/S9_design_patterns_with_typescript/src/classes/User.ts
+++ src/S9_design_patterns_with_typescript/src/classes/User.ts
@@ -1,3 +1,17 @@
-class User {}
+import faker from 'faker';
+import { Coordinates } from '../types/index';
+
+class User {
+  public name: string;
+  public location: Coordinates;
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+}
 
 export default User;
