c948e38dd147c1d634bcfa8c751269a5211e18f0
Manu Hdez <manu.hdez.dev@gmail.com>
2019-11-25 23:42:00 +0000

    defining a company

--- src/S9_design_patterns_with_typescript/src/classes/Company.ts
+++ src/S9_design_patterns_with_typescript/src/classes/Company.ts
@@ -1,3 +1,19 @@
-class Company {}
+import faker from 'faker';
+import { Coordinates } from '../types/index';
+
+class Company {
+  public name: string;
+  public catchPhrase: string;
+  public location: Coordinates;
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+}
 
 export default Company;
--- src/S9_design_patterns_with_typescript/src/index.ts
+++ src/S9_design_patterns_with_typescript/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there');
+import User from './classes/User';
+import Company from './classes/Company';
+// import Map from './classes/Map'
+
+const testUser = new User();
+const testCompany = new Company();
+
+console.log({ testUser, testCompany });
