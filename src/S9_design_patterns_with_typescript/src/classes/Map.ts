b7537951f0c6b4c0e079145af137a5647e77372f
Manu Hdez <manu.hdez.dev@gmail.com>
2019-11-26 00:16:59 +0000

    added custom map

--- src/S9_design_patterns_with_typescript/src/classes/Map.ts
+++ src/S9_design_patterns_with_typescript/src/classes/Map.ts
@@ -1,3 +1,14 @@
-class Map {}
+import { Coordinates } from '../types/index';
+
+class Map {
+  private googleMap: google.maps.Map;
+
+  constructor(wrapper: HTMLElement | null, center: Coordinates) {
+    this.googleMap = new google.maps.Map(wrapper, {
+      zoom: 5,
+      center: center
+    });
+  }
+}
 
 export default Map;
--- src/S9_design_patterns_with_typescript/src/index.ts
+++ src/S9_design_patterns_with_typescript/src/index.ts
@@ -1,8 +1,11 @@
 import User from './classes/User';
 import Company from './classes/Company';
-// import Map from './classes/Map'
+import Map from './classes/Map';
 
 const testUser = new User();
 const testCompany = new Company();
 
 console.log({ testUser, testCompany });
+
+const mapContainer = document.getElementById('map-root');
+new Map(mapContainer, testUser.location);
