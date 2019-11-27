7aaa2473abf906e5dd72edac5eddc4b2aba05b53
Manu Hdez <manu.hdez.dev@gmail.com>
2019-11-27 23:22:56 +0000

    sorter scaffolding

--- src/S10_more_on_design_patterns/src/index.ts
+++ src/S10_more_on_design_patterns/src/index.ts
@@ -1,1 +1,17 @@
-console.log('hi there!');
+class Sorter {
+  public collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {}
+
+  printCollection(): void {
+    console.log(`Sorted collection: ${this.collection}`);
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+sorter.printCollection();
