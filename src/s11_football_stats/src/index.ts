82e73841e8f17604a3604387be9616eea9003380
Manu Hdez <manu.hdez.dev@gmail.com>
2019-12-08 17:12:27 +0000

    running a simple analysis

--- src/s11_football_stats/src/index.ts
+++ src/s11_football_stats/src/index.ts
@@ -4,3 +4,15 @@ const matches = fs
   .readFileSync('football.csv', { encoding: 'utf-8' })
   .split('\n')
   .map((row: string): string[] => row.split(','));
+
+let manUnitedWins = 0;
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log({ manUnitedWins });
