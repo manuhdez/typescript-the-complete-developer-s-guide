4b2e10e147da2db8528c2fa7a1b68be73c76a658
Manu Hdez <manu.hdez.dev@gmail.com>
2020-01-01 18:18:03 +0000

    basic express routes

--- src/s14_express/src/index.ts
+++ src/s14_express/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import express, { Response, Request } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>hello world</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
+});
