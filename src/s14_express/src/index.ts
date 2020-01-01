981283be6370e8c6f4c8798583a7e760a76c4ab0
Manu Hdez <manu.hdez.dev@gmail.com>
2020-01-01 18:48:15 +0000

    parsing form body

--- src/s14_express/src/index.ts
+++ src/s14_express/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+// routes
 import { authRouter } from './routes/authRoutes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(authRouter);
 
 app.listen(3000, () => {
--- src/s14_express/src/routes/authRoutes.ts
+++ src/s14_express/src/routes/authRoutes.ts
@@ -1,11 +1,34 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, response } from 'express';
 
 export const authRouter = Router();
 
 authRouter.get('/', (req: Request, res: Response) => {
-  res.send(`
+  res.status(200).send(`
     <div>
       <h1>hello world</h1>
     </div>
   `);
+});
+
+authRouter.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <h3>Login</h3>
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" required />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" required />
+      </div>
+      <button type="submit">Log in</button>
+    </form>
+  `);
+});
+
+authRouter.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  res.status(200).send(email + password);
 });
