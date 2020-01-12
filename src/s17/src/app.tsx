022f8af58fc7172cd35f6af91552446d98a24140
Manu Hdez <manu.hdez.dev@gmail.com>
2020-01-12 01:59:55 +0000

    fetching todos from app component

--- src/s17/src/app.tsx
+++ src/s17/src/app.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+// components
 import Counter from './components/Counter';
 
-interface AppProps {
-  name?: string;
+// redux
+import { Dispatch } from 'redux';
+import { StoreState } from './store/reducer';
+import { fetchTodos, Todo } from './store/todos/actions';
+
+interface AppState {
+  todos: Todo[];
 }
 
-export default class App extends React.Component<AppProps> {
+interface AppActions {
+  getTodos: () => Promise<void>;
+}
+
+interface AppProps extends AppState, AppActions {}
+
+class App extends React.Component<AppProps> {
+  componentDidMount() {
+    const { getTodos } = this.props;
+    getTodos();
+  }
+
   render() {
     return (
       <div>
-        <h3>hello {this.props.name || 'invited'}</h3>
         <Counter />
+        {/* <TodosList todos={todos} /> */}
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): AppState => ({
+  todos: todos.todos
+});
+
+const mapDispatchToProps = (dispatch: Dispatch): AppActions => ({
+  getTodos: () => dispatch<any>(fetchTodos())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
